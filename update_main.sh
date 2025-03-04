0827817529c0cf03d0f45db0612fa8d2cc6ba27a
thanhnt <13204600+thanhnguyentang@users.noreply.github.com>
2025-03-04 17:44:08 -0500

    update

---
+++ update_main.sh
@@ -0,0 +1,10 @@
+python2 jemdoc.py index.jemdoc
+python2 jemdoc.py paper.jemdoc
+python2 jemdoc.py teaching.jemdoc
+# python2 jemdoc.py homework.jemdoc
+# python2 jemdoc.py lectures.jemdoc
+# python2 jemdoc.py reference.jemdoc
+# python2 jemdoc.py project.jemdoc
+
+# rsync -rvz www/*.html www/jemdoc.css yc5@nobel.princeton.edu:public_html/ele520_math_data/
+  
